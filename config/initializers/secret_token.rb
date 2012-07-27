0470108f92440d6192369516896eb508ec22f954
Jérôme Izard <shidame@free.fr>
2012-07-27 16:00:49 +0200

    Init Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TerresDuNil::Application.config.secret_token = '6c3da363844516721a7b77811340343670b4bc7fade3b2423ad53a163fe3841b7f91994589892a0f2b94c4b7e494d42caf093db42d4596efe58d182df6ee4cb8'
